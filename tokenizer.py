b65c366bcada7232cd42274747d7a1975c9e1dda
Jack Hagen <jjhags@gmail.com>
2025-11-20 21:19:26 -0600

    Added requirements file, wrote tokenizers for bi/trigrams and prefixes

---
+++ tokenizer.py
@@ -0,0 +1,83 @@
+import nltk
+import string
+
+from nltk.util import bigrams
+from nltk.util import trigrams
+
+nltk.download('punkt', quiet=True)
+
+def tokenize_bigrams(text):
+    """
+    Tokenizes the input text into bigrams
+
+    Parameters:
+    text (str): The input text to be tokenized
+
+    Returns:
+    list: A list of bigram tuples
+    """
+
+    grams=[]
+
+    for i in text:
+        grams.append(list(bigrams(i)))
+
+    return grams[1:]
+
+def tokenize_trigrams(text):
+    """
+    Tokenizes the input text into trigrams
+    
+    Paramenters:
+    text (list): The input text to be tokenized
+    
+    Returns:
+    list: A list of trigram tuples
+    """
+
+    grams=[]
+
+    for i in text:
+        grams.append(list(trigrams(i)))
+
+    return grams[1:]
+
+def tokenize_prefixes(text, n):
+    """
+    Tokenizes the input text into prefixes of length n
+
+    Parameters:
+    text (str): The input text to be tokenized
+    n (int): The length of the prefixes
+
+    Returns:
+    list: A list of prefix strings
+    """
+
+    prefixes = []
+
+    for word in text:
+        if len(word) >= n:
+            prefixes.append(word[:n])
+        else:
+            prefixes.append(word)
+
+    return [item for item in prefixes if len(item) == n]
+
+def clean(text):
+    """
+    Cleans the input text by removing punctuation and converting to lowercase
+
+    Parameters:
+    text (str): The input text to be cleaned
+
+    Returns:
+    cleaned text (list): The cleaned text split into words
+    """
+
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text.split()
+
+
+#print(tokenize_prefixes(clean("I love programming in python"), 3))
